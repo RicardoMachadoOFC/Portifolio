506aa46eb1c895c1e236d6752f1745c23d02ccac
RicardoMachadoOFC <cado.machado@hotmail.com>
2025-08-25 18:02:15 -0300

    Adicionando primeira versão do portifolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mensagem no console
+window.onload = () => {
+  console.log("Portfólio do Ricardo carregado com sucesso!");
+};
+
+// Alternar modo escuro
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
